208a252c7cc989888ce68c4a1662cd596ef547ce
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-06-16 19:19:54 -0300

    Tela de cadastro de usuários - Trabalhando

---
+++ app/controllers/cadastro.js
@@ -0,0 +1,22 @@
+module.exports.cadastro = function(application, req, res){
+    res.render('cadastro', {validation: {}});
+}
+
+module.exports.cadastrar = function (application, req, res){
+    var dadosform = req.body;
+
+    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa não pode ser vazio').notEmpty();
+
+   var erros = req.validationErrors();
+
+    if(erros){
+        res.render('cadastro', {validacao: erros});
+        return;
+    }
+
+    res.send('podemos cadastrar');
+
+}
